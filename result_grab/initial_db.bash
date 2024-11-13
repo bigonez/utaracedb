3892e7ac553190081a7db349b36de731ece97bd4
Jun Zhang <bigonez@gmail.com>
2024-11-13 15:59:47 +1100

    Rename, duplicate& simplify database.
    . change the database name, initial_db.bash, uta100_2024.sql & uta100_data_grab.py;
    . duplicate and simplify the database, post_data_grab.bash & post_data_grab.sql;
        uta100_2024.db3 -> uta100_nodes.db3

--- result_grab/initial_db.bash
+++ result_grab/initial_db.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # initial the tables for UTA100 race data
-sqlite3 uta100_nodes.db3 < uta100_nodes.sql
+sqlite3 uta100_2024.db3 < uta100_2024.sql
 
 # assign the write permission to the group user
-chmod g+w uta100_nodes.db3
+chmod g+w uta100_2024.db3
